d027245a996d012e2d3a00a5dbf18b693228c49c
vinaneosoft <vinaneosoft@gmail.com>
2025-05-28 11:31:20 +0530

    commit

---
+++ Javascript/Script.js
@@ -0,0 +1,16 @@
+/* dynamically typed language : data types are dynamically assigned depends on value */
+
+let s1="hello";
+var s2="neosoft"
+const s3=56;
+var s2="rabale"; /* var type variables can be redeclared */
+//let s1=567; /* let type variables can not be redeclared */
+
+
+
+let n1="one hundred";
+console.log(typeof n1); //string
+n1=100;
+console.log(typeof n1);// number
+n1=true;
+console.log(typeof n1); //boolean
