f51ed0f60a167de7c79cf26a16de90cc34f3eda1
vinaneosoft <vinaneosoft@gmail.com>
2025-05-30 13:51:52 +0530

    commit

---
+++ JQUERY/external.js
@@ -0,0 +1,11 @@
+$(function(){
+     $('#btn').click(function(){
+        console.log($("#india").html()); // innerHTML
+        console.log($("#india").text()); // innerText
+        console.log($("#company").val()); // value
+        console.log($("#company").prop('value')); // .property
+    })
+})
+
+//$(document).ready();
+//$();
